bb1698f2fb00f8fec0fc8241afff78f482e8391d
SincerelyBrittany <sincerelybr@gmail.com>
2021-02-07 21:25:26 -0500

    creates onclick for hog name

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class HelloWorld extends Component {
+  handleClick = () => {
+    console.log("here");
+  };
+
   render() {
     const pigImageStub = require("../hog-imgs/" +
       this.props.name.toLowerCase().split(" ").join("_") +
@@ -8,7 +12,7 @@ class HelloWorld extends Component {
     return (
       <div className="pigTile">
         <h3>{this.props.name}</h3>
-        <img src={pigImageStub} alt={this.props.name} />
+        <img src={pigImageStub} alt={this.props.name} onClick={handleClick} />
       </div>
     );
   }
